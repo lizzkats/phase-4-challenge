081e7ed45d501c0ee12934cd5fc065f70eb7308b
lizzkats <katsnelson.lizz@gmail.com>
2017-06-20 12:47:48 -0700

    adds logic for passport and a partial for header and nav

--- passport.js
+++ passport.js
@@ -1,17 +1,17 @@
-const LocalStrategy  = require('passport-local').Strategy;
-const User = require('./database.js')
+const LocalStrategy  = require('passport-local').Strategy
+const user = require('./database.js')
 
 module.exports = function(passport) {
 
     passport.serializeUser(function(user, done) {
-        done(null, user.id);
-    });
+        done(null, user.id)
+    })
 
     passport.deserializeUser(function(id, done) {
-        User.getUserbyID(id, function(err, user) {
-            done(err, user);
-        });
-    });
+        User.findById(id, function(err, user) {
+            done(err, user)
+        })
+    })
 
     passport.use('local-signup', new LocalStrategy({
         usernameField : 'email',
@@ -22,23 +22,45 @@ module.exports = function(passport) {
 
         process.nextTick(function() {
 
-        User.getUsers({ 'local.email' :  email }, function(err, user) {
+        User.findOne({ 'local.email' :  email }, function(err, user) {
             if (err)
-                return done(err);
+                return done(err)
             if (user) {
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                return done(null, false, req.flash('signupMessage', 'That email is already taken.'))
             } else {
-                const newUser = new User();
-                newUser.local.email = email;
-                newUser.local.password = password;
+                const newUser = new User()
+                newUser.local.email = email
+                newUser.local.password = password
 
                 newUser.save(function(err) {
                     if (err)
-                        throw err;
-                    return done(null, newUser);
-                });
+                        throw err
+                    return done(null, newUser)
+                })
             }
-        });
-        });
-    }));
-};
+
+        })
+
+        })
+
+    }))
+    const localLoginOptions = {
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }
+    passport.use('local-login', new LocalStrategy(localLoginOptions, configLocalLogin))
+
+    function configLocalLogin (req, email, password, done) {
+        User.getUsers({ 'local.email': email }, function (err, user) {
+            if (err) { return done(err) }
+            if (!user) {
+                return done(null, false, req.flash('loginMessage', 'No user found.'))
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'))
+            }
+            return done(null, user)
+        })
+    }
+  }
